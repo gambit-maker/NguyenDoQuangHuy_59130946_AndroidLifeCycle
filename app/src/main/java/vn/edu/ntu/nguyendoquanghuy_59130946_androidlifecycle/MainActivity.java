7e9a4d0a28ba45b504dd7970adacac0d2b7a0498
HP <quanghuy12c6@gmail.com>
2020-04-15 22:00:28 +0700

    V1.4

--- app/src/main/java/vn/edu/ntu/nguyendoquanghuy_59130946_androidlifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/nguyendoquanghuy_59130946_androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package vn.edu.ntu.nguyendoquanghuy_59130946_androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +11,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Testing
-        //Testing 2
-        //Testing 3
-        //Testing 4
+        Log.d("LifeCycle","onCreate");
+
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d("LifeCycle","onStart");
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d("LifeCyle","onRestart");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d("LifeCycle","onResume");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d("LifeCycle","onPause");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d("LifeCycle","onStop");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy");
     }
 }
